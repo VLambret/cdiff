ce1a29e201a4dc31cbf1dcfc83aeba9a0a960bf2
Victor Lambret <lambret.victor@gmail.com>
2018-09-02 21:26:36 +0200

    [REFACTOR] Change indentation for tabs

--- src/main.c
+++ src/main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-    if (argc != 3) {
-        return 1;
-    }
-    return 0;
+	if (argc != 3) {
+		return 1;
+	}
+
+	return 0;
 }
--- test/test.sh
+++ test/test.sh
@@ -6,25 +6,25 @@ TMP_FILE=.cdiff_test.tmp
 FILE1=test/samples/helloworld.txt
 
 teardown() {
-    rm -f $TMP_FILE
+	rm -f $TMP_FILE
 }
 
 @test "Given no parameters Cdiff fails" {
-    run $CDIFF > /dev/null
-    [ $status -ne 0 ]
+	run $CDIFF > /dev/null
+	[ $status -ne 0 ]
 }
 
 @test "Given a single file Cdiff fails" {
-    run $CDIFF $FILE1 > /dev/null
-    [ $status -ne 0 ]
+	run $CDIFF $FILE1 > /dev/null
+	[ $status -ne 0 ]
 }
 
 @test "Given two existing file names Cdiff does not fail" {
-    run $CDIFF $FILE1 $FILE1 > /dev/null
-    [ $status -eq 0 ]
+	run $CDIFF $FILE1 $FILE1 > /dev/null
+	[ $status -eq 0 ]
 }
 
 @test "Given more than two files Cdiff fails" {
-    run $CDIFF $FILE1 $FILE1 $FILE1 > /dev/null
-    [ $status -ne 0 ]
+	run $CDIFF $FILE1 $FILE1 $FILE1 > /dev/null
+	[ $status -ne 0 ]
 }
