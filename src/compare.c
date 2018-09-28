a8987ac2cd36c5b3c83b246212a45d88e5d4e5ed
Victor Lambret <lambret.victor@gmail.com>
2018-09-28 20:17:01 +0200

    [TEST] Allow multiple patterns in test case for Identical Lines

--- src/compare.c
+++ src/compare.c
@@ -2,5 +2,5 @@
 #include <compare.h>
 
 void line_compare(const char *line1, const char *line2, char *result) {
-	strcpy(result, "toto");
+	strcpy(result, line1);
 }
--- test/compare_test.c
+++ test/compare_test.c
@@ -1,15 +1,35 @@
 #include "unity_src/unity.h"
 #include <compare.h>
 
-void test_givenTwoIdenticalLines_Then_OutputIsLineContent() {
-	char result[MAX_LINE_SIZE];
-	line_compare("toto", "toto", result);
-	TEST_ASSERT_EQUAL_STRING("toto", result);
+#define END_OF_LINE_PATTERNS {.reference = NULL, .compared = NULL, .expected = NULL}
 
+struct line_patterns {
+	char * reference;
+	char * compared;
+	char * expected;
+};
+
+void loop_over_test_cases(struct line_patterns *p) {
+	while(p->reference) {
+		char result[MAX_LINE_SIZE];
+		line_compare(p->reference, p->compared, result);
+		TEST_ASSERT_EQUAL_STRING(p->expected, result);
+		p++;
+	}
+}
+
+void test_ThereIsNoDifferenceBetweenTwoIdenticalLines() {
+	struct line_patterns givens[] = {
+		{.reference = "", .compared = "", .expected = ""},
+		{.reference = "toto", .compared = "toto", .expected = "toto"},
+		{.reference = "tata", .compared = "tata", .expected = "tata"},
+		END_OF_LINE_PATTERNS
+	};
+	loop_over_test_cases(givens);
 }
 
 int main() {
 	UnityBegin("Compare tests");
-	RUN_TEST(test_givenTwoIdenticalLines_Then_OutputIsLineContent);
+	RUN_TEST(test_ThereIsNoDifferenceBetweenTwoIdenticalLines);
 	return UnityEnd();
 }
