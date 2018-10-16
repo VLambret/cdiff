0a55649986f6e23483ac6f5d29ade92cbee17750
Victor Lambret <lambret.victor@gmail.com>
2018-10-16 15:19:38 +0200

    [DEV] Create result buffer according to expected result size

--- src/main.c
+++ src/main.c
@@ -1,14 +1,26 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 #include "compare.h"
 
+#define COLOR_OVERHEAD_SIZE 10
+
 int main(int argc, char **argv) {
-	char result[4096];
+	char *result;
 	if (argc != 3) {
-		return 1;
+		fprintf(stderr, "usage : %s string1 string2", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	result = malloc((strlen(argv[1]) + strlen(argv[2])) * COLOR_OVERHEAD_SIZE);
+	if (!result) {
+		perror("Cannot allocate memory for result");
+		exit(EXIT_FAILURE);
 	}
 
 	line_compare(argv[1], argv[2], result);
 	printf("%s\n", result);
+	free(result);
 
 	return 0;
 }
